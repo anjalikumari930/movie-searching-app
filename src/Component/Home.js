4af4b877bd8d5a541428143e8571e2acd4625a96
anjalikumari930 <anjalikumari93045@gmail.com>
2024-02-18 23:23:01 +0530

    final commit

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,37 +1,39 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Moviecard from "./moviecard";
 
 const Home = () => {
   const [Movie, setMovie] = useState([]);
   const [Search, setSearch] = useState("");
   const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=cfaa0483";
-  const fetchmovie = async (title) => {
-    const response = await fetch(`${apiurl}&s=${title}`);
-    const data = await response.json();
 
-    setMovie(data.Search);
+  const fetchMovie = async (title) => {
+    try {
+      const response = await axios.get(`${apiurl}&s=${title}`);
+      setMovie(response.data.Search);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
   };
-  const handlesearch = (e) => {
+
+  const handleSearch = (e) => {
     setSearch(e.target.value);
   };
+
   useEffect(() => {
     if (Search.trim() !== "") {
-      fetchmovie(Search);
+      fetchMovie(Search);
     } else {
-      setMovie([]); // Clear movies if search term is empty
+      setMovie([]);
     }
   }, [Search]);
-
-  console.log(Movie);
-
-  //console.log(Search);
 
   return (
     <div className="container">
       <h1>Movie Searching</h1>
       <div className="search">
         <input
-          onChange={handlesearch}
+          onChange={handleSearch}
           value={Search}
           placeholder="Type movie name"
         ></input>
